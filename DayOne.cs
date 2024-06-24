e4b3ddbedf6ebc233acde5fd254cda96c8bc53be
MrFrozenNoodles <55391939+FrozenMonk1234@users.noreply.github.com>
2024-06-24 21:39:44 +0200

    Add project files.

---
+++ DayOne.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    public class DayOne
+    {
+        
+        public int PartOne(string input)
+        {
+            int[] numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+            string[] inputList = input.Split(Environment.NewLine);
+            int[] calibrationValues = new int[inputList.Length];
+            for (int i = 0; i < inputList.Length; i++)
+            {
+                var calibratedValue = "";
+                var reversedTemp = inputList[i].Reverse().ToList();
+                var temp = inputList[i].ToList();
+                for (int j = 0; j < temp.Count; j++)
+                {
+                    if (calibratedValue.Length == 1)
+                    {
+                        break;
+                    }
+
+                    for (int k = 0; k < numbers.Length; k++)
+                    {
+                        if (temp[j].ToString().Equals(numbers[k].ToString()))
+                        {
+                            calibratedValue += numbers[k];
+                            break;
+                        }
+                    }
+                }
+
+                for (int j = 0; j < reversedTemp.Count; j++)
+                {
+                    if (calibratedValue.Length == 2)
+                    {
+                        break;
+                    }
+
+                    for (int k = 0; k < numbers.Length; k++)
+                    {
+                        if (reversedTemp[j].ToString().Equals(numbers[k].ToString()))
+                        {
+                            calibratedValue += numbers[k];
+                            break;
+                        }
+                    }
+                }
+
+                Int32.TryParse(calibratedValue, out int value);
+                calibrationValues[i] = value;
+            }
+
+            return calibrationValues.Sum();
+        }
+    }
+}
---
+++ DayTwo.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    internal class DayTwo
+    {
+
+        public int Execute(string input)
+        {
+            string[] colors = ["red", "green", "blue"];
+            List<string> inputList = input.Split(Environment.NewLine).ToList();
+            List<string> outputList = inputList;
+
+            for (int i = 0; i < inputList.Count; i++)
+            {
+                var game = inputList[i].Substring(0, 7);
+                var set = inputList[i].Substring(7);
+
+                var charArray = set.ToCharArray();
+                bool redFlag = false;
+                Dictionary<string, int> invalidGame = new();
+                for (int j = 0; j < charArray.Length; j++)
+                {
+                    if (charArray[j] == 'r' && charArray[j + 1] == 'e' && charArray[j + 2] == 'd')
+                    {
+                        var Number = string.Empty;
+                        for (int k = j - 3; k <= j - 1; k++)
+                        {
+                            Number += charArray[k];
+                        }
+
+                        Int32.TryParse(Number.Trim(), out var value);
+                        if (value > 12)
+                        {
+                            redFlag = true;
+                            break;
+                        }
+                    }
+
+                    if (charArray[j] == 'g' && charArray[j + 1] == 'r' && charArray[j + 2] == 'e')
+                    {
+                        var Number = string.Empty;
+                        for (int k = j - 3; k <= j - 1; k++)
+                        {
+                            Number += charArray[k];
+                        }
+
+                        Int32.TryParse(Number.Trim(), out var value);
+                        if (value > 13)
+                        {
+                            redFlag = true;
+                            break;
+                        }
+                    }
+
+                    if (charArray[j] == 'b' && charArray[j + 1] == 'l' && charArray[j + 2] == 'u')
+                    {
+                        var Number = string.Empty;
+                        for(int k = j - 3; k <= j -1; k++)
+                        {
+                            Number += charArray[k];
+                        }
+
+                        Int32.TryParse(Number.Trim(), out var value);
+                        if (value > 13)
+                        {
+                            redFlag = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (redFlag)
+                {
+                    outputList.RemoveAt(i);
+                }
+            }
+
+            int result = 0;
+            for(int i = 0; i < outputList.Count; i++)
+            {
+                var tempList = outputList[i].Split(':');
+
+                if (int.TryParse(tempList[0].Replace("Game",""), out int value))
+                {
+                    result += value;
+                }
+            }
+
+            return result;
+        }
+    }
+}
